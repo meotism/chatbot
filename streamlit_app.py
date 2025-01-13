c041ae10a68115c4c8aa1fce0b04278b84f47e85
meotism <57476209+meotism@users.noreply.github.com>
2025-01-13 04:28:55 +0000

    change flow chatbot response

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,8 +96,7 @@ with input_container:
         text = speech_to_text(language='vi', start_prompt="⏺️", stop_prompt="⏹️", key="STT", use_container_width=True, just_once=True)
     if text:
         # st.session_state.input = text
-        user_input = text
-        flow_response(user_input)
+        st.session_state.input_text = text
     with c1:
         input_text = st.text_input("You: ", value="", key="input", on_change=submit, label_visibility= "collapsed")
         user_input = st.session_state.input_text
